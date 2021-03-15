2b85dd04e167b50210f67f3eabba575322c1c87b
jebinjeb <j.jebin1990@gmail.com>
2021-03-15 15:44:50 +0530

    generate random numbers

--- solution/gencsv.sh
+++ solution/gencsv.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+cat /dev/null > inputFile 
 dne=10 #number of entries default value
 number_of_entries=${1:-$dne} 
 for i in $(seq 1 $number_of_entries)
 do
-	echo $i
+	NUMBER=$[ ( $RANDOM % $number_of_entries )  + 1 ]
+	echo `expr $i - 1`, $NUMBER >> inputFile
 done
